a4a3b798e219d39849d043d4f42dec7c0a98e71f
augustobarradas <112783194+augustobarradas@users.noreply.github.com>
2023-09-14 08:24:02 -0300

    tentar tirar os espacos

--- main.js
+++ main.js
@@ -9,3 +9,40 @@ botao.addEventListener("input", function() {
 
 });
 
+if (texto.trim() === " ") {
+    contador.textContent = totalLetras - 1
+}
+
+function contarLetrasSemEspacos() {
+    for (var i = 0; i < totalLetras; i++) {
+      if (texto[i] !== ' ') {
+        contador++;
+      }
+    }
+}
+
+
+
+
+
+// function contarLetrasSemEspacos(frase) {
+    // Dividir a frase em um array de caracteres
+    //var caracteres = frase.split('');
+  
+    // Inicializar uma variável para contar as letras
+    //var contador = 0;
+  
+    // Iterar pelos caracteres e contar apenas as letras (ignorando espaços em branco)
+    //for (var i = 0; i < caracteres.length; i++) {
+      //if (caracteres[i] !== ' ') {
+        //contador++;
+      //}
+    //}
+  
+    //return contador;
+  //}
+  
+  // Exemplo de uso
+  //var frase = "Esta é uma frase de exemplo";
+  //var numeroDeLetras = contarLetrasSemEspacos(frase);
+  //console.log("Número de letras (sem contar espaços): " + numeroDeLetras);
